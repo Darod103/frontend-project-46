fc1cf3d5d061fcbbdf81ff5b54961d11578ed324
Darod103 <darod103@gmail.com>
2024-02-24 10:08:17 +1000

    part2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,10 @@ program
     .version('0.0.1')
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .usage('[options] <filepath1> <filepath2>')
+
+
     
 
 
